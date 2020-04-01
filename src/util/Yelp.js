2ed28acc6eb0b4b9362cc59a20a7d7cd4239f094
Ricky Soo <8564646+rickysoo@users.noreply.github.com>
2020-04-01 21:52:59 +0800

    Comnplete Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,10 +1,32 @@
 const apiKey = 'x0IwEwk9ERo7iw7YWm7-K3e5Br8BtLJD2Qb0Kd-BKIaX7jExYGq7GvM0RPIr92pXucmWIe2ZLbhMkPId2gLHe7FxnIb2Rq8ntYR4sdpK001EcsWyPQP1FM2rGHmEXnYx';
 
 const Yelp = {
-
+    search(term, location, sortBy) {
+        const url = 'https://cors-anywhere.herokuapp.com/' + `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    
+        return fetch(url, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0]['title'],
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                });
+            }
+        });
+    }
 };
 
-function search(term, location, sortBy) {
-    return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, );
-}
-
+export default Yelp;
