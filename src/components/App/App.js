889bd8bb223392a644aaceab3dcd49758fcf8ba5
Ricky Soo <8564646+rickysoo@users.noreply.github.com>
2020-04-02 11:40:06 +0800

    Connect to Yelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,80 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    alert(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      businesses: []
+    };
+    
+    this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    alert(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+
+    Yelp.search(term, location, sortBy)
+    .then(businesses => {
+// alert(businesses);
+      this.setState({
+        businesses: businesses
+      });
+    });
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
+
+/*
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -24,21 +97,4 @@ const businesses = [
   business,
   business
 ];
-
-class App extends React.Component {
-  searchYelp(term, location, sortBy) {
-    alert(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
-      </div>
-    );
-  }
-}
-
-export default App;
+*/
